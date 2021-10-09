3ac834fdbd5f717d019889bdfbee377720d77c12
Rogalowski <rogalowski@gmail.com>
2021-10-09 12:58:21 +0200

    #2 Strona główna - Dynamiczne statystyki - Fundation/Organizations/Local Donators - ADDED PAGINATOR PAGE

--- charity_good_hands_app/views.py
+++ charity_good_hands_app/views.py
@@ -2,13 +2,14 @@ import random
 
 from random import randint
 
+from django.core.paginator import Paginator
 from django.db.models import Sum
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 
-from charity_good_hands_app.models import Donation, Institution, Category
+from charity_good_hands_app.models import Donation, Institution
 
 
 class LandingPageView(View):
@@ -17,65 +18,49 @@ class LandingPageView(View):
         bag_quantity = Donation.objects.all().aggregate(Sum('quantity'))['quantity__sum']
         institution_quantity = Donation.objects.all().annotate(Sum('institution'))
 
-
-        #
-        # list_random_num = [0]
-        # while 0 in list_random_num:
-        #     list_random_num = random.sample(range(6), 3)
-
         institution = Institution.objects.filter(type=1)
         organization = Institution.objects.filter(type=2)
         local_donator = Institution.objects.filter(type=3)
-
 
         list_institution = []
         list_organization = []
         list_local_donator = []
 
+        '''Can we get next page without reloading/switch page (without loading new data from database) ?'''
         while len(list_institution) != len(institution):
             r = random.choice(institution)
             if r not in list_institution:
                 list_institution.append(r)
+
+        paginator = Paginator(institution, 4)
+        page = request.GET.get('page')
+        list_institution_pages = paginator.get_page(page)
+
 
         while len(list_organization) != len(organization):
             r = random.choice(organization)
             if r not in list_organization:
                 list_organization.append(r)
 
+        paginator = Paginator(organization, 4)
+        page = request.GET.get('page')
+        list_organization_pages = paginator.get_page(page)
+
         while len(list_local_donator) != len(local_donator):
             r = random.choice(local_donator)
             if r not in list_local_donator:
                 list_local_donator.append(r)
 
-
-        print(list_institution)
-        # random_list = random.choices(random1, k=3)
-        # random_list = random.choices(random1, k=4)
-
-
-
-        # institution1 = list_institution[1]
-        # institution2 = list_institution[2]
-        # institution3 = list_institution[0]
-        # institution3 = random.choice(random1)
-        # institution2 = Institution.objects.get(id=list_random_num[-2])
-        # institution3 = Institution.objects.get(id=list_random_num[-3])
-
-        # institution3 = Institution.objects.get(id=d.index(1))
-        # institution3 = Institution.objects.get(id=randint(1, last_inst))
-        # institution = Institution.objects.all()[randint(0, last_inst - 1)]
-
-
+        paginator = Paginator(local_donator, 4)
+        page = request.GET.get('page')
+        list_local_donator_pages = paginator.get_page(page)
 
         context = {
             'bag_quantity': bag_quantity,
             'institution_quantity': institution_quantity,
-            # 'institution1': institution1,
-            # 'institution2': institution2,
-            # 'institution3': institution3,
-            'list_institution': list_institution,
-            'list_organization': list_organization,
-            'list_local_donator': list_local_donator,
+            'list_institution_pages': list_institution_pages,
+            'list_organization_pages': list_organization_pages,
+            'list_local_donator_pages': list_local_donator_pages,
         }
         return render(request, 'index.html', context)
 
